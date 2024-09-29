b35704fdfc0be92b29d3c875cff6948383e3b400
Devulpally-Sindhu <sindhu09.dev@gmail.com>
2024-09-30 00:02:08 +0530

    changes-on-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sindhu_angular_app';
+  firstname="devulapally sindhu";
+  num1=100;
+  num2=200;
+  num3:any="sindhu";
+  count=0;
+
+  students=["kavya","karim","Jasmin","naresh","murari"];
+
+  
+
+  people = [
+    {
+    "firstname": "praveen",
+    "lastname": "gubbala",
+    "age": 36,
+    "gender": "male",
+    "city": "hyd",
+    "salary": 10000
+    },
+    {
+    "firstname": "srikanth",
+    "lastname": "gubbala",
+    "age": 32,
+    "gender": "male",
+    "city": "bengaluru",
+    "salary": 20000
+    },
+    {
+    "firstname": "pradeep",
+    "lastname": "reddy",
+    "age": 21,
+    "gender": "male",
+    "city": "hyd",
+    "salary": 30000
+    },
+    {
+    "firstname": "mounika",
+    "lastname": "mudiraj",
+    "age": 20,
+    "gender": "female",
+    "city": "nalgonda",
+    "salary": 30000
+    },
+    {
+    "firstname": "nikhil",
+    "lastname": "m",
+    "age": 22,
+    "gender": "male",
+    "city": "guntur",
+    "salary": 2000
+    },
+    {
+    "firstname": "riya",
+    "lastname": "bhadouria",
+    "age": 14,
+    "gender": "female",
+    "city": "indore",
+    "salary": 40000
+    }
+    ];
+
+   
+   
 }
